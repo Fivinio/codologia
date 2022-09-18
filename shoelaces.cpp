a3c14c7cea9456a981a10a1fdf1cdf708a5b23b4
Fivinio <112808029+Fivinio@users.noreply.github.com>
2022-09-18 11:04:03 +0300

    Update shoelaces.cpp

--- shoelaces.cpp
+++ shoelaces.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 using namespace std;
 int func(int a, int b, int l, int n) {
-  cout<<2*l+(2*n-1)*a;\
+  cout<<c=2*l+(2*n-1)*a;
   return 0;
  }
 int main () {
   func(5,4,10,6);
+  cin>>c>>endl;
   return 0;
 }
